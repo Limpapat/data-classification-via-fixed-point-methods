8a0822106cdd4fd5d272b070e0a3a1f7b9735391
Limpapat Bussaban <lim.bussaban@gmail.com>
2022-06-22 21:08:24 +0700

    add gensetting

--- gencseq.py
+++ gencseq.py
@@ -5,57 +5,6 @@ cseq = {
     "theta" : "sqrt(1+4*n*n)/(1+sqrt(1+4*(n+1)*(n+1)))"
 }
 
-# setting = {
-#     "data" : "source/sample_generated_data.csv",
-#     "model" : "MLP",
-#     "loss" : "MCE",
-#     "penalty" : "l1",
-#     "n_test" : .1,
-#     "n_iter" : 100,
-#     "optimizers" : {
-#         "SGD" : {
-#             "lr" : 2.,
-#             "rp" : 1e-3,
-#             "cseq" : None
-#         },
-#         "FBA" : {
-#             "lr" : 2.,
-#             "rp" : 1e-3,
-#             "cseq" : './cseq.json'
-#         },
-#         "SFBA" : {
-#             "lr" : 2.,
-#             "rp" : 1e-3,
-#             "cseq" : './cseq.json'
-#         },
-#         "ISFBA" : {
-#             "lr" : 2.,
-#             "rp" : 1e-3,
-#             "cseq" : './cseq.json'
-#         },
-#         "PFBA" : {
-#             "lr" : 2.,
-#             "rp" : 1e-3,
-#             "cseq" : './cseq.json'
-#         },
-#         "IPFBA" : {
-#             "lr" : 2.,
-#             "rp" : 1e-3,
-#             "cseq" : './cseq.json'
-#         },
-#         "ParallelSFBA" : {
-#             "lr" : 2.,
-#             "rp" : 1e-3,
-#             "cseq" : './cseq.json'
-#         },
-#         "ParallelISFBA" : {
-#             "lr" : 2.,
-#             "rp" : 1e-3,
-#             "cseq" : './cseq.json'
-#         },
-#     }
-# }
-
 with open('cseq.json', 'w') as f:
     f.write(json.dumps(cseq))
     f.close()
---
+++ gensetting.py
@@ -0,0 +1,56 @@
+import json
+
+setting = {
+    "data" : "source/sample_generated_data.csv",
+    "model" : "MLP",
+    "loss" : "MCE",
+    "penalty" : "l1",
+    "n_test" : .1,
+    "n_iter" : 100,
+    "optimizers" : {
+        "SGD" : {
+            "lr" : 2.,
+            "rp" : 1e-3,
+            "cseq" : None
+        },
+        "FBA" : {
+            "lr" : 2.,
+            "rp" : 1e-3,
+            "cseq" : './cseq.json'
+        },
+        "SFBA" : {
+            "lr" : 2.,
+            "rp" : 1e-3,
+            "cseq" : './cseq.json'
+        },
+        "ISFBA" : {
+            "lr" : 2.,
+            "rp" : 1e-3,
+            "cseq" : './cseq.json'
+        },
+        "PFBA" : {
+            "lr" : 2.,
+            "rp" : 1e-3,
+            "cseq" : './cseq.json'
+        },
+        "IPFBA" : {
+            "lr" : 2.,
+            "rp" : 1e-3,
+            "cseq" : './cseq.json'
+        },
+        "ParallelSFBA" : {
+            "lr" : 2.,
+            "rp" : 1e-3,
+            "cseq" : './cseq.json'
+        },
+        "ParallelISFBA" : {
+            "lr" : 2.,
+            "rp" : 1e-3,
+            "cseq" : './cseq.json'
+        },
+    }
+}
+
+with open('setting.json', 'w') as f:
+    f.write(json.dumps(setting))
+    f.close()
