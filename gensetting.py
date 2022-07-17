5b373ff77420940f1a13fa5b9a4c1105cbd70ea7
Limpapat Bussaban <lim.bussaban@gmail.com>
2022-07-17 11:02:49 +0700

    prepare major update

--- gensetting.py
+++ gensetting.py
@@ -2,33 +2,33 @@ import json
 
 setting = {
     "data" : [
-        {
-            "name" : "sample_generated_data",
-            "path" : "source/sample_generated_data.csv",
-            "class_label" : ["class_1", "class_2", "class_3"]
-        },
+        # {
+        #     "name" : "sample_generated_data",
+        #     "path" : "source/sample_generated_data.csv",
+        #     "class_label" : ["class_1", "class_2", "class_3"]
+        # },
         {
             "name" : "iris",
             "path" : "source/iris.data",
             "feature_selection" : [0, 1]
         },
-        {
-            "name" : "abalone",
-            "path" : "source/abalone.data",
-            "feature_selection" : [1, 2],
-            "target" : 0
-        },
-        {
-            "name" : "parkinsons",
-            "path" : "source/parkinsons.data",
-            "feature_selection" : [1, 2],
-            "target" : 17
-        },
-        {
-            "name" : "heart_disease",
-            "path" : "source/heart.csv",
-            "feature_selection" : [0, 3]
-        },
+        # {
+        #     "name" : "abalone",
+        #     "path" : "source/abalone.data",
+        #     "feature_selection" : [1, 2],
+        #     "target" : 0
+        # },
+        # {
+        #     "name" : "parkinsons",
+        #     "path" : "source/parkinsons.data",
+        #     "feature_selection" : [1, 2],
+        #     "target" : 17
+        # },
+        # {
+        #     "name" : "heart_disease",
+        #     "path" : "source/heart.csv",
+        #     "feature_selection" : [0, 3]
+        # },
         ],
     "model" : "MLP",
     "loss" : "MCE",
@@ -47,36 +47,36 @@ setting = {
             "rp" : 1e-3,
             "cseq" : './cseq.json'
         },
-        "SFBA" : {
-            "lr" : 2.,
-            "rp" : 1e-3,
-            "cseq" : './cseq.json'
-        },
-        "ISFBA" : {
-            "lr" : 2.,
-            "rp" : 1e-3,
-            "cseq" : './cseq.json'
-        },
-        "PFBA" : {
-            "lr" : 2.,
-            "rp" : 1e-3,
-            "cseq" : './cseq.json'
-        },
-        "IPFBA" : {
-            "lr" : 2.,
-            "rp" : 1e-3,
-            "cseq" : './cseq.json'
-        },
-        "ParallelSFBA" : {
-            "lr" : 2.,
-            "rp" : 1e-3,
-            "cseq" : './cseq.json'
-        },
-        "ParallelISFBA" : {
-            "lr" : 2.,
-            "rp" : 1e-3,
-            "cseq" : './cseq.json'
-        },
+        # "SFBA" : {
+        #     "lr" : 2.,
+        #     "rp" : 1e-3,
+        #     "cseq" : './cseq.json'
+        # },
+        # "ISFBA" : {
+        #     "lr" : 2.,
+        #     "rp" : 1e-3,
+        #     "cseq" : './cseq.json'
+        # },
+        # "PFBA" : {
+        #     "lr" : 2.,
+        #     "rp" : 1e-3,
+        #     "cseq" : './cseq.json'
+        # },
+        # "IPFBA" : {
+        #     "lr" : 2.,
+        #     "rp" : 1e-3,
+        #     "cseq" : './cseq.json'
+        # },
+        # "ParallelSFBA" : {
+        #     "lr" : 2.,
+        #     "rp" : 1e-3,
+        #     "cseq" : './cseq.json'
+        # },
+        # "ParallelISFBA" : {
+        #     "lr" : 2.,
+        #     "rp" : 1e-3,
+        #     "cseq" : './cseq.json'
+        # },
     }
 }
 
